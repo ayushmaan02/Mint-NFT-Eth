135d3fab9136df564acc4ccc9d7704c68f775ac7
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-06-26 00:01:29 +0530

    Updated TokenURI

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,25 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('HorseNFT');
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
+  const nftContractFactory = await hre.ethers.getContractFactory('HorseNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract Deployed to:", nftContract.address);
   
-    // Call the function.
-    let txn = await nftContract.makeAnHorseNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #1")
-  
-    txn = await nftContract.makeAnHorseNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #2")
-  };
-  
-  const runMain = async () => {
-    try {
+  //Calling the NFT Function
+  let txn = await nftContract.makeAnHorseNFT()
+  await txn.wait()
+  console.log("Minted #1")
+};
+
+
+const runMain = async () => {
+  try {
       await main();
       process.exit(0);
-    } catch (error) {
+  } catch (error) {
       console.log(error);
       process.exit(1);
-    }
-  };
-  
-  runMain();
+      
+  }
+};
+
+runMain();
